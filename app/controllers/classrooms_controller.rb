35d174ac533942dd7228a41af4367dcbc76cfdaa
Romeo Gono <romeogono1@aol.com>
2019-07-21 13:56:11 +0000

    Automatically backed up by Learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,9 @@
 class ClassroomsController < ApplicationController
   def show
-    @classroom = Classroom.find(params[:id])
-    @student = @classroom.oldest_student
-  end
+   @classroom = Classroom.find(params[:id])
+ end
 
-  def index
-    @classrooms = Classroom.all
-  end
+ def index
+   @classrooms = Classroom.all
+ end
 end
